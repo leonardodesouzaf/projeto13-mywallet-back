f021e084bfe142d688bc1a92a412abfd8a75f7ab
Leonardo de Souza Ferreira <leonardosouzaf@icloud.com>
2022-09-10 22:13:09 -0300

    in and out done

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import dotenv from 'dotenv';
 import { v4 as uuid } from 'uuid';
 import joi from 'joi';
 import bcrypt from 'bcrypt';
+import dayjs from 'dayjs';
 
 dotenv.config();
 
@@ -58,6 +59,14 @@ app.post('/sign-up', async (req,res) => {
 app.post("/sign-in", async (req, res) => {
     try {
             const user = req.body;
+            const userSchema = joi.object({
+                password: joi.string().required(),
+                email: joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required()
+            });
+            const validation = userSchema.validate(user, { abortEarly: true });
+            if (validation.error) {
+                return res.status(422).send('Digite os seus dados corretamente!');
+            }
             const userDB = await db.collection('users').findOne({ email: user.email });
             if(userDB && bcrypt.compareSync(user.password, userDB.password)) {
                 const token = uuid();
@@ -88,8 +97,8 @@ app.get('/home', async function (req,res) {
         }
         const user = await db.collection("users").findOne({ _id: session.userId })
         if(user) {
-            await db.collection("transactions").find().toArray().then(usersArray => {
-                return res.send(usersArray);
+            await db.collection("transactions").find({ userId: session.userId }).toArray().then(usersArray => {
+                return res.status(201).send(usersArray);
             });
         } else {
             return res.status(401).send('Login não autorizado!'); 
@@ -99,10 +108,82 @@ app.get('/home', async function (req,res) {
      }
 });
 
+app.post("/in", async (req, res) => {
+    try {
+            const { authorization } = req.headers;
+            const token = authorization?.replace('Bearer ', '');
+            if(!token) return res.status(401).send('Login não autorizado!');
+            const session = await db.collection("sessions").findOne({ token });       
+            if (!session) {
+                return res.status(401).send('Login não autorizado!'); 
+            }
+            const user = await db.collection("users").findOne({ _id: session.userId })
+            if(user) {
+                const transaction = req.body;
+                const transactionSchema = joi.object({
+                    description: joi.string().required(),
+                    value: joi.number().required()
+                });
+                const validation = transactionSchema.validate(transaction, { abortEarly: true });
+                if (validation.error) {
+                    return res.status(422).send('Digite os seus dados corretamente!');
+                }
+                db.collection('transactions').insertOne(
+                    { 
+                        value: transaction.value,
+                        description: transaction.description,
+                        date: dayjs().format('DD/MM'),
+                        userId: session.userId,
+                        type: 'in'
+                    }
+                );
+                return res.status(201).send('Transação efetuada!');
+            } else {
+                return res.status(401).send('Login não autorizado!'); 
+            }
+        }
+    catch (error) {
+        return res.status(500).send('Não foi possível conectar ao servidor!');
+    }
+});
 
-
-
-
-
+app.post("/out", async (req, res) => {
+    try {
+            const { authorization } = req.headers;
+            const token = authorization?.replace('Bearer ', '');
+            if(!token) return res.status(401).send('Login não autorizado!');
+            const session = await db.collection("sessions").findOne({ token });       
+            if (!session) {
+                return res.status(401).send('Login não autorizado!'); 
+            }
+            const user = await db.collection("users").findOne({ _id: session.userId })
+            if(user) {
+                const transaction = req.body;
+                const transactionSchema = joi.object({
+                    description: joi.string().required(),
+                    value: joi.number().precision(2).required()
+                });
+                const validation = transactionSchema.validate(transaction, { abortEarly: true });
+                if (validation.error) {
+                    return res.status(422).send('Digite os seus dados corretamente!');
+                }
+                db.collection('transactions').insertOne(
+                    { 
+                        value: transaction.value,
+                        description: transaction.description,
+                        date: dayjs().format('DD/MM'),
+                        userId: session.userId,
+                        type: 'out'
+                    }
+                );
+                return res.status(201).send('Transação efetuada!');
+            } else {
+                return res.status(401).send('Login não autorizado!'); 
+            }
+        }
+    catch (error) {
+        return res.status(500).send('Não foi possível conectar ao servidor!');
+    }
+});
 
 app.listen(5000);
